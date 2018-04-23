422817977b339e38e8b692c9317f1aebb58e44c7
Rick Zhou <rinick@gmail.com>
2018-04-23 00:30:47 -0700

    unit test on js block nested function

--- src/engine/logic/script/spec/Js.spec.ts
+++ src/engine/logic/script/spec/Js.spec.ts
@@ -3,27 +3,31 @@ import "../Js";
 import { Job, Root } from "../../../core/Job";
 
 describe("Script", () => {
-    it('basic', () => {
-        let job = new Job();
+  it('basic', () => {
+    let job = new Job();
 
-        let aBlock = job.createBlock('a');
+    let aBlock = job.createBlock('a');
 
-        aBlock.setValue('#class', 'js');
-        aBlock.setValue('script', 'this["out1"] = 321');
-        Root.run();
+    aBlock.setValue('#class', 'js');
+    aBlock.setValue('script', 'this["out1"] = 321');
+    Root.run();
 
-        assert.equal(aBlock.getValue('out1'), 321, 'basic script output');
-    });
+    assert.equal(aBlock.getValue('out1'), 321, 'basic script output');
+  });
 
-    it('nested function', () => {
-        let job = new Job();
+  it('nested function', () => {
+    let job = new Job();
 
-        let aBlock = job.createBlock('a');
+    let aBlock = job.createBlock('a');
 
-        aBlock.setValue('#class', 'js');
-        aBlock.setValue('script', 'return function(){this["out2"] = 456}');
-        Root.run();
+    aBlock.setValue('#class', 'js');
+    aBlock.setValue('script', 'let temp = 456; return function(){this["out2"] = ++temp;}');
 
-        assert.equal(aBlock.getValue('out2'), 456, 'basic script output');
-    });
+    Root.run();
+    assert.equal(aBlock.getValue('out2'), 457, 'nested function script output');
+
+    aBlock.updateValue('#call', {});
+    Root.run();
+    assert.equal(aBlock.getValue('out2'), 458, 'nested function script local value');
+  });
 });
