c23cea77521a64fdea9a870f2d161c7af3ba369e
Rick Zhou <rinick@gmail.com>
2018-04-30 23:55:30 -0700

    save nested job

--- src/core/nested/spec/NestedJob.spec.ts
+++ src/core/nested/spec/NestedJob.spec.ts
@@ -14,15 +14,20 @@ describe("NestedJob", () => {
 
     aBlock.setValue('#class', '/NestedJob/class1');
 
-    NestedJob.registerClass('/NestedJob/class1',
-      {
-        'runner': {'#class': 'test-runner', '@log': 'nest1', '~#call': '##.#input.in1'}
-      });
+    let jobData = {
+      'runner': {'#class': 'test-runner', '@log': 'nest1', '~#call': '##.#input.in1'}
+    };
+    NestedJob.registerClass('/NestedJob/class1', jobData);
 
     Root.run();
     assert.deepEqual(TestFunctionRunner.logs, ['nest1'],
       'nested job should be created');
     TestFunctionRunner.clearLog();
+
+    let impl: Job = aBlock.getValue('#impl')  as Job;
+    assert.instanceOf(impl, Job, 'get #impl of nested job');
+
+    assert.deepEqual(impl.save(), jobData, 'serialize nested job');
 
     aBlock.setValue('in1', 1);
     Root.run();
@@ -33,7 +38,8 @@ describe("NestedJob", () => {
     aBlock.setValue('in1', 2);
     aBlock.setValue('#class', null);
     Root.run();
-    assert.isEmpty(TestFunctionRunner.logs,
-      'nested job destroy');
+    assert.isEmpty(TestFunctionRunner.logs, 'nested job destroy');
+
+    assert.deepEqual(impl.save(), jobData, 'serialize nested job after destroy');
   });
 });
