ad72773c1db1b81766edd98e4bf8ce95f46d3544
Rick Zhou <rinick@gmail.com>
2018-06-24 22:27:46 -0700

    add void property

---
+++ src/engine/block/Void.ts
@@ -0,0 +1,57 @@
+import { BlockProperty, BlockPropertyEvent, BlockPropertySubscriber } from "./BlockProperty";
+import { Listener } from "./Dispatcher";
+
+
+class VoidProperty extends BlockProperty {
+  static readonly insteance = new VoidProperty(null, '');
+
+  onChange(val: any): boolean {
+    return false;
+  }
+
+  setValue(val: any) {
+    // do nothing
+  }
+
+  setBinding(path: string) {
+    // do nothing
+  }
+
+  _save(): any {
+    throw new Error("Can not save destroyed property");
+  }
+
+  _load(val: any) {
+    throw new Error("Can not load destroyed property");
+  }
+
+  _liveUpdate(val: any) {
+    throw new Error("Can not liveUpdate destroyed property");
+  }
+
+  subscribe(subscriber: BlockPropertySubscriber) {
+    // do nothing
+  }
+
+  unsubscribe(subscriber: BlockPropertySubscriber) {
+    // do nothing
+  }
+
+  addEvent(event: BlockPropertyEvent) {
+    // do nothing
+  }
+
+  listen(listener: Listener<any>) {
+    // do nothing
+  }
+
+  unlisten(listener: Listener<any>) {
+    // do nothing
+  }
+
+  destroy() {
+    // do nothing
+  }
+}
+
+export const voidProperty = VoidProperty.insteance;
---
+++ src/engine/block/spec/Void.spec.ts
@@ -0,0 +1,43 @@
+import { assert } from "chai";
+import { voidProperty } from "../Void";
+
+import { Job } from "../Job";
+import { BlockPropertyEvent } from "../BlockProperty";
+import { Dispatcher } from "../Dispatcher";
+
+describe("VoidProperty", () => {
+
+  it('basic', () => {
+
+    voidProperty.setValue(1);
+    assert.isUndefined(voidProperty.getValue(), 'void property never change value');
+    voidProperty.updateValue(2);
+    voidProperty.setBinding('a');
+    assert.isUndefined(voidProperty.getValue(), 'void property never change value');
+
+    voidProperty.listen({
+      onSourceChange(prop: Dispatcher<any>) {
+        /* istanbul ignore next */
+        throw new Error('should not be called');
+      },
+      onChange(val: any) {
+        /* istanbul ignore next */
+        throw new Error('should not be called');
+      }
+    });
+    assert.isEmpty(voidProperty._listeners, 'void property wont listen');
+
+    voidProperty.subscribe({
+      onPropertyEvent(change: BlockPropertyEvent) {
+        /* istanbul ignore next */
+        throw new Error('should not be called');
+      }
+    });
+    assert.isUndefined(voidProperty._subscribers, 'void property wont subscribe');
+
+    assert.throw(() => voidProperty._save(), 'Can not save destroyed property');
+    assert.throw(() => voidProperty._load({}), 'Can not load destroyed property');
+    assert.throw(() => voidProperty._liveUpdate({}), 'Can not liveUpdate destroyed property');
+  });
+
+});
