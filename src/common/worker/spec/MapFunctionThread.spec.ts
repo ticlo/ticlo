f67661c74fdba32a3edfadf0a4da05c2594c9a53
Rick Zhou <rinick@gmail.com>
2018-07-31 21:46:59 -0700

    async worker test on thread mode MapFunction

--- src/common/worker/spec/MapFunctionThread.spec.ts
+++ src/common/worker/spec/MapFunctionThread.spec.ts
@@ -66,4 +66,55 @@ describe("MapFunction Thread", () => {
     }
   });
 
+  it('async worker', async () => {
+    TestFunctionRunner.clearLog();
+    let job = new Job();
+
+    job.setValue('a', inputObj);
+
+    let bBlock = job.createBlock('b');
+
+    bBlock._load({
+      '#is': 'map',
+      'thread': 5,
+      '~input': '##.a',
+      'src': {
+        '#is': {
+          '#is': '',
+          'async': {'#is': 'async-function-promise', '~#call': '##.#input'},
+          'add': {'#is': 'add', '#mode': 'onCall', '~#call': '##.async.#emit', '~0': '##.#input', '1': 1},
+          '~#waiting': 'async.#waiting',
+          '~#output': 'add.output'
+        }
+      }
+    });
+
+    Root.run();
+    assert.isUndefined(bBlock.getValue('output'), 'async worker should not finish right after run');
+
+    let output = await bBlock.waitValue('output');
+    assert.lengthOf(Object.keys(output), 20);
+
+    for (let i = 0; i < 20; ++i) {
+      assert.equal(output['v' + i], i + 1);
+    }
+    job.setValue('a', {
+      'v1': 1,
+      'v2': 4,
+      'v4': 5
+    });
+
+    output = await bBlock.waitNextValue('output');
+
+    assert.deepEqual(output, {'v1': 2, 'v2': 5, 'v4': 6}, 'input change');
+
+    job.setValue('a', inputArr);
+    let outputArr = await bBlock.waitNextValue('output');
+    assert.lengthOf(outputArr, 20, 'output array');
+
+    for (let i = 0; i < 20; ++i) {
+      assert.equal(outputArr[i], i + 101);
+    }
+  });
+
 });
