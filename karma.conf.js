eef7bfd66fd19db4f4999baaed04aa78bd24feb6
Rick Zhou <rinick@gmail.com>
2018-08-07 23:45:40 -0700

    fix karma coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,14 @@
 module.exports = function (config) {
     config.set({
         frameworks: ["mocha"],
-        // preprocessors: {
-        //     'build/**/!(*spec).js': ['coverage']
-        // },
+        preprocessors: {
+            'build/**/*.js': ['coverage']
+        },
         files: [
             'build/specs.js'
         ],
-        plugins: ['karma-remap-istanbul', 'karma-mocha', 'karma-chrome-launcher'],
-        reporters: ['dots', 'karma-remap-istanbul'],
+        plugins: ['karma-remap-istanbul', 'karma-mocha', 'karma-coverage', 'karma-chrome-launcher'],
+        reporters: ['dots', 'coverage', 'karma-remap-istanbul'],
         remapIstanbulReporter: {
             reports: {
                 "lcovonly": ".karma_coverage/lcov.txt"
