8662089acf502c5b179a79422d2774593469a372
Rick Zhou <rinick@gmail.com>
2018-08-08 23:41:21 -0700

    fix file path in remapIstanbulReporter

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,15 @@
+let fixPathRegWin = /^\.\.\\test\/[\w]:\\/;
+let fixPathLinux = /^\.\.\/test\/home\//;
+
+function mapFileName(str) {
+    if (fixPathRegWin.test(str)) {
+        return str.substr(8);
+    } else if (fixPathLinux.test(str)) {
+        return str.substr(7);
+    }
+    return str;
+}
+
 module.exports = function (config) {
     config.set({
         frameworks: ["mocha"],
@@ -9,9 +21,17 @@ module.exports = function (config) {
         ],
         plugins: ['karma-remap-istanbul', 'karma-mocha', 'karma-coverage', 'karma-chrome-launcher'],
         reporters: ['dots', 'coverage', 'karma-remap-istanbul'],
+        coverageReporter: {
+            dir: '.karma_coverage',
+            reporters: [] // no default report
+        },
         remapIstanbulReporter: {
             reports: {
                 "lcovonly": ".karma_coverage/lcov.txt"
+            },
+            remapOptions: {
+                mapFileName,
+                exclude: /node_module/
             }
         },
         singleRun: true,
