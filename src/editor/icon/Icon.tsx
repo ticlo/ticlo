16ffa49a91bf9cf8b9a25f0a6dcc16a960115d6c
Rick Zhou <rinick@gmail.com>
2018-09-12 19:44:58 -0700

    fix custom icon

--- src/editor/icon/Icon.tsx
+++ src/editor/icon/Icon.tsx
@@ -1,18 +1,34 @@
 import * as React from "react";
 
+const measureTable = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  5, 8, 13, 9, 15, 13, 4, 7, 7, 9, 9, 4, 8, 4, 9, 11, 7, 11, 11, 11, 10, 10, 10, 10, 10, 4, 4, 11, 9, 11, 9, 16, 14, 12, 12, 13, 11, 11, 13, 13, 5, 10, 12, 11, 16, 13, 14, 11, 15, 11, 10, 13, 13, 14, 18, 13, 12, 12, 6, 10, 6, 9, 15, 9, 11, 11, 9, 11, 10, 8, 11, 11, 5, 5, 10, 6, 15, 11, 11, 10, 10, 8, 9, 8, 11, 11, 14, 10, 11, 11, 7, 4, 7, 12];
+
 interface Props {
   icon: string;
-  priority?: number;
+  priority?: string | number;
 }
 
+// not an accurate way to measure a string
 function iconNameLength(str: string): number {
-  return 18;
+  let size = str.length;
+  let width = 0;
+  for (let i = 0; i < size; ++i) {
+    let code = str.charCodeAt(i);
+    if (code < 32) {
+      return -1;
+    }
+    if (code > 126) {
+      return size * 18;
+    }
+    width += measureTable[code];
+  }
+  return width;
 }
 
 export function TIcon(props: Props) {
   let {icon, priority} = props;
   let priClass = '';
-  if (priority >= 0) {
+  if (priority != null) {
     priClass = `tico-pr${priority}`;
   }
   // TODO priority for repeater and group
@@ -23,21 +39,22 @@ export function TIcon(props: Props) {
       switch (iconType) {
         case 'fas':
         case 'fab':
-          return (<div className={`tico ${iconType} fa-${iconName} ${priClass}`}/>);
+          return (<div className={`tico ${iconType} fa-${iconName} ${priClass}`} />);
         case 'txt':
           if (iconName.length > 3) {
             iconName = iconName.substr(0, 3);
           }
           let nameLen = iconNameLength(iconName);
-          if (nameLen <= 18) {
-            return (<div className={`tico tico-txt ${priClass}`}>{iconName}</div>);
-          } else {
-            let fontSize = Math.floor(18 * 18 / nameLen);
-            return (<div className={`tico tico-txt ${priClass}`} style={{fontSize}}>{iconName}</div>);
+          if (nameLen > 0) {
+            if (nameLen <= 22) {
+              return (<div className={`tico tico-txt ${priClass}`}>{iconName}</div>);
+            } else {
+              let fontSize = Math.floor(18 * 22 / nameLen);
+              return (<div className={`tico tico-txt ${priClass}`} style={{fontSize}}>{iconName}</div>);
+            }
           }
-
       }
     }
   }
-  return <div className="tico"/>;
+  return <div className="tico" />;
 }
