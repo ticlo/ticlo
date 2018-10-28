3ace2f95296e6219e64d5b5146799765b3ce5563
Rick Zhou <rinick@gmail.com>
2018-10-28 01:34:52 -0700

    more i18n tests

--- src/common/util/spec/i18n.spec.ts
+++ src/common/util/spec/i18n.spec.ts
@@ -5,7 +5,7 @@ import * as i18next from "i18next"
 describe("i18n", function () {
 
   before(async function () {
-    i18n.init('en');
+    await i18n.init('en');
     i18next.addResourceBundle('en', 'ticlo-testi18n', {
       'aaa': {
         '$': 'AAA',
@@ -18,12 +18,17 @@ describe("i18n", function () {
 
   it('class name', function () {
 
+    assert.equal(i18n.transLateClass(null), '');
     assert.equal(i18n.transLateClass('aaa'), 'aaa');
 
+    assert.equal(i18n.transLateClass(null, 'testi18n'), '');
     assert.equal(i18n.transLateClass('aaa', 'testi18n'), 'AAA');
   });
 
   it('property name', function () {
+
+    assert.equal(i18n.transLateProperty('aaa', ''), '');
+    assert.equal(i18n.transLateProperty('', 'bbb'), '');
 
     assert.equal(i18n.transLateProperty('aaa', 'bbb'), 'bbb');
     assert.equal(i18n.transLateProperty('aaa', 'bbb1'), 'bbb1');
