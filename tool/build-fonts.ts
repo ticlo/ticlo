0b2e9bf1c856c54ab9e5b469f982509c33b187bf
Rick Zhou <rinick@gmail.com>
2018-11-04 23:04:54 -0800

    add icon files

---
+++ tool/build-fonts.ts
@@ -0,0 +1,67 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as glob from "glob";
+import {exec} from 'child_process';
+
+
+async function async_exec(cmd: string) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, {cwd: __dirname, timeout: 100000}, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve();
+    });
+  });
+
+}
+
+async function main() {
+  let output: string[] = [];
+
+  let materialIcons: string[] = glob.sync(`icons/material/ic_*_48px.svg`);
+  for (let path of materialIcons) {
+    let data = fs.readFileSync(path, 'utf8').replace('<path ', '<path fill="#FFF" ')
+      .replace(/</g, '%3C').replace(/>/g, '%3D');
+    let name = path.substring(path.lastIndexOf('/ic_') + 4, path.length - 9);
+    output.push(`
+.tico-material-${name} {
+  background: transparent url('data:image/svg+xml,${data}') 50% 50% / contain no-repeat;
+}`);
+  }
+
+  let faIcons: string[] = glob.sync(`icons/font-awesome/**/*.svg`);
+  for (let path of faIcons) {
+    let paths = path.split('/');
+    let fileName = paths[paths.length - 1];
+    let name = fileName.substr(0, fileName.length - 4);
+    let folderName = paths[paths.length - 2];
+    let group: string;
+    switch (folderName) {
+      case 'solid':
+        group = 'fas';
+        break;
+      case 'brands':
+        group = 'fab';
+        break;
+      case 'regular':
+        group = 'fa';
+        break;
+      default:
+        continue;
+    }
+    let data = fs.readFileSync(path, 'utf8').replace('<path ', '<path fill="#FFF" ')
+      .replace(/</g, '%3C').replace(/>/g, '%3D');
+
+    output.push(`
+.tico-${group}-${name} {
+  background: transparent url('data:image/svg+xml,${data}') 50% 50% / contain no-repeat;
+}`);
+  }
+
+
+  fs.writeFileSync('dist/fonts.css', output.join('\n'));
+}
+
+main();
