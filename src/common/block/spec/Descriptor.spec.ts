c12bbd7b14b2e09ef75cbbeca33a10dc34996916
Rick Zhou <rinick@gmail.com>
2018-12-15 21:18:41 -0800

    test getFuncStyleFromDesc

---
+++ src/common/block/spec/Descriptor.spec.ts
@@ -0,0 +1,11 @@
+import {assert} from "chai";
+import {getFuncStyleFromDesc} from "../Descriptor";
+
+describe("Descriptor", function () {
+
+  it('getFuncStyleFromDesc', function () {
+    assert.equal(getFuncStyleFromDesc({style: 'repeater', priority: 1, name: 't1'}), 'ticl-block-prr');
+    assert.equal(getFuncStyleFromDesc({priority: 1, name: 't2'}), 'ticl-block-pr1');
+    assert.equal(getFuncStyleFromDesc({name: 't3'}), '');
+  });
+});
