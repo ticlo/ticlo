446c86a0b51d498a11679504416a1437ca666784
Rick Zhou <rinick@gmail.com>
2018-12-17 18:54:17 -0800

    test save load of object with #is

--- src/common/block/spec/Property.spec.ts
+++ src/common/block/spec/Property.spec.ts
@@ -1,6 +1,27 @@
 import {assert} from "chai";
 import {Job} from "../Block";
 
-describe("Property", function () {
-  // TODO
+describe("Property Save Load", function () {
+  it('save object with #is', function () {
+    let job = new Job();
+
+    let v1Data = {'#is': 'add'};
+    let expectedSave = {
+      '#is': '',
+      'v1': {
+        '#is': {'#is': 'add'}
+      }
+    };
+
+    job.setValue('v1', v1Data);
+
+    let saved = job.save();
+    assert.deepEqual(saved, expectedSave);
+
+    job.load(expectedSave);
+    assert.deepEqual(job.getValue('v1'), v1Data);
+
+    job.liveUpdate(expectedSave);
+    assert.deepEqual(job.getValue('v1'), v1Data);
+  });
 });
