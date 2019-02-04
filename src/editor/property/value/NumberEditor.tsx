beb52602a1a80744fb1cfb8ff6ea34ac512d5b39
Rick Zhou <rinick@gmail.com>
2019-02-04 01:20:32 -0800

    dont commit value during user typing

--- src/editor/property/value/NumberEditor.tsx
+++ src/editor/property/value/NumberEditor.tsx
@@ -10,7 +10,14 @@ interface Props {
 
 export class NumberEditor extends React.Component<Props, any> {
 
+  _commitedValue: number;
+  _pendingValue: number;
 
+  constructor(props: Props) {
+    super(props);
+    this._commitedValue = props.value;
+    this._pendingValue = props.value;
+  }
 
   shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<any>, nextContext: any) {
     let props = this.props;
@@ -20,16 +27,55 @@ export class NumberEditor extends React.Component<Props, any> {
     if (props.onChange !== nextProps.onChange) {
       return true;
     }
-    if (!Object.is(props.value, nextProps.value)){
-
+    if (!Object.is(props.value, nextProps.value)) { // Object.is also covers NaN
+      // only render when there is no pendingValue
+      return Object.is(this._pendingValue, this._commitedValue);
     }
+    return false;
   }
+
+  commitChange(value: number) {
+    this._pendingValue = value;
+    this._commitedValue = value;
+    this.props.onChange(value);
+  }
+
+  onValueChange = (value: any) => {
+    if (this._pendingTyping) {
+      this._pendingValue = value;
+      this._pendingTyping = false;
+    } else {
+      this.commitChange(value);
+    }
+  };
+
+  onBlur = () => {
+    if (!Object.is(this._pendingValue, this._commitedValue)) { // Object.is also covers NaN
+      this.commitChange(this._pendingValue);
+    }
+  };
+
+  _pendingTyping = false;
+  onKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      this._pendingTyping = false;
+      if (!Object.is(this._pendingValue, this._commitedValue)) { // Object.is also covers NaN
+        this.commitChange(this._pendingValue);
+      }
+    } else if (e.key === 'Esc') {
+      this._pendingTyping = false;
+      this._pendingValue = this._commitedValue;
+    } else {
+      this._pendingTyping = true;
+    }
+  };
 
   render() {
-    let {desc, value, onChange} = this.props;
+    let {desc, value} = this.props;
     return (
-      <InputNumber size='small' placeholder={desc.placeholder} min={desc.min} max={desc.max} step={desc.step} defaultValue={value} onChange={onChange}/>
+      <InputNumber size='small' placeholder={desc.placeholder} value={value}
+                   min={desc.min} max={desc.max} step={desc.step}
+                   onChange={this.onValueChange} onBlur={this.onBlur} onKeyDown={this.onKeyDown}/>
     );
   }
-
 }
