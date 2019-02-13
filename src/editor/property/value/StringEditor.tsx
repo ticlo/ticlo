44d32091969d37ed205b142874638da6cb842a27
Rick Zhou <rinick@gmail.com>
2019-02-13 00:22:42 -0800

    remove use of antd TextArea internal ref

--- src/editor/property/value/StringEditor.tsx
+++ src/editor/property/value/StringEditor.tsx
@@ -4,76 +4,35 @@ import {PropDesc} from "../../../common/block/Descriptor";
 
 const {TextArea} = Input;
 
-interface TextAreaRef {
-  textAreaRef: HTMLTextAreaElement;
-
-  resizeOnNextFrame(): void;
-}
-
 interface Props {
   value: any;
   desc: PropDesc;
   onChange: (value: any) => void;
 }
 
-interface State {
-  value: any;
-}
+export class StringEditor extends React.PureComponent<Props, any> {
 
-export class StringEditor extends React.Component<Props, any> {
-
-  _serverValue: string;
-  _pendingValue: string = null;
-
-  _textAreaRef: TextAreaRef;
-
-  getTextArea = (ref: any) => {
-    this._textAreaRef = ref;
-  };
+  // this is not a state bacause in commitChange() editorValue is changed but we don't want a re-render until prop change
+  _pendingValue: any = null;
 
   constructor(props: Props) {
     super(props);
-    this._serverValue = props.value;
-  }
-
-  shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<any>, nextContext: any) {
-    let props = this.props;
-    if (props.value !== nextProps.value) {
-      this._serverValue = nextProps.value;
-      // only render when there is no pendingValue (pending is NaN)
-      if (this._pendingValue == null) {
-        if (this._textAreaRef.textAreaRef) {
-          this._textAreaRef.textAreaRef.value = nextProps.value;
-          this._textAreaRef.resizeOnNextFrame();
-        }
-      }
-    }
-    if (props.desc !== nextProps.desc) {
-      return true;
-    }
-    if (props.onChange !== nextProps.onChange) {
-      return true;
-    }
-
-    return false;
   }
 
   commitChange(value: string) {
     this._pendingValue = null;
-    this._serverValue = value;
     this.props.onChange(value);
   }
 
   onValueChange = (e: React.SyntheticEvent) => {
     let value = (e.nativeEvent.target as HTMLTextAreaElement).value;
     if (this._pendingTyping) {
-      if (typeof value === 'string') {
-        if (value !== this._serverValue || this._pendingValue != null) {
-          // when pending value already exists or server value is not the same
-          this._pendingValue = value;
-        }
+      if (value !== this.props.value || this._pendingValue != null) {
+        // when editorValue value already exists or server value is not the same
+        this._pendingValue = value;
       }
       this._pendingTyping = false;
+      this.forceUpdate();
     } else {
       this.commitChange(value);
     }
@@ -89,8 +48,8 @@ export class StringEditor extends React.Component<Props, any> {
   onKeyDown = (e: React.KeyboardEvent) => {
     if (e.key === 'Escape') {
       if (this._pendingValue != null) {
-        this._textAreaRef.textAreaRef.value = this._serverValue;
         this._pendingValue = null;
+        this.forceUpdate();
       }
       return;
     } else if (e.key === 'Enter' && !(e.shiftKey)) {
@@ -107,11 +66,10 @@ export class StringEditor extends React.Component<Props, any> {
 
   render() {
     let {desc, value, onChange} = this.props;
-    if (this._pendingValue != null) {
-      value = this._pendingValue;
-    }
+    if (this._pendingValue != null) value = this._pendingValue;
     return (
-      <TextArea ref={this.getTextArea} placeholder={desc.placeholder} defaultValue={value} disabled={onChange == null}
+      <TextArea placeholder={desc.placeholder} value={value}
+                disabled={onChange == null}
                 autosize={{minRows: 1, maxRows: 5}}
                 onChange={this.onValueChange} onBlur={this.onBlur} onKeyDown={this.onKeyDown}/>
     );
