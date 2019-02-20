af249cb31e4828cc6a8eadb767dc27e94909972c
Rick Zhou <rinick@gmail.com>
2019-02-20 01:01:02 -0800

    right click menu on property name

--- src/editor/property/PropertyEditor.tsx
+++ src/editor/property/PropertyEditor.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Tooltip} from "antd";
+import {Button, Tooltip, Dropdown, Menu, Input} from "antd";
 import {ClientConnection, ValueState, ValueUpdate} from "../../common/connect/ClientConnection";
 import {FunctionDesc, PropDesc} from "../../common/block/Descriptor";
 import {translateProperty} from "../../common/util/i18n";
@@ -15,6 +15,8 @@ import {PasswordEditor} from "./value/PasswordEditor";
 import {ExpandIcon} from "../../ui/component/Tree";
 import {PropertyList} from "./PropertyList";
 import {arrayEqual} from "../../common/util/Compare";
+
+const {SubMenu} = Menu;
 
 const typeEditorMap: {[key: string]: any} = {
   'number': NumberEditor,
@@ -66,6 +68,24 @@ interface State {
   showSubBlock: boolean;
 }
 
+interface PropertyState {
+  count: number;
+  value?: any;
+  valueSame: boolean;
+  bindingPath?: string;
+  hasBinding: boolean;
+  bindingSame: boolean;
+  subBlock: boolean;
+}
+
+const notReadyState = {
+  count: 0,
+  valueSame: false,
+  hasBinding: false,
+  bindingSame: false,
+  subBlock: false,
+};
+
 export class PropertyEditor extends MultiSelectComponent<Props, State, PropertyLoader> {
 
   constructor(props: Readonly<Props>) {
@@ -102,6 +122,12 @@ export class PropertyEditor extends MultiSelectComponent<Props, State, PropertyL
     let {conn, keys, name} = this.props;
     for (let key of keys) {
       conn.setValue(`${key}.${name}`, value);
+    }
+  };
+  onUnbindClick = (e: any) => {
+    let {conn, keys, name} = this.props;
+    for (let key of keys) {
+      conn.setBinding(`${key}.${name}`, undefined);
     }
   };
 
@@ -158,117 +184,140 @@ export class PropertyEditor extends MultiSelectComponent<Props, State, PropertyL
     DragStore.dragEnd();
   };
 
+  getPropertyState(): PropertyState {
+    let it = this.loaders[Symbol.iterator]();
+    let [firstKey, firstLoader] = it.next().value;
+    let firstCache = firstLoader.cache;
+    if (!firstCache) {
+      return notReadyState;
+    }
+
+    let count = this.loaders.size;
+    let value = firstCache.value;
+    let valueSame = true;
+    let bindingPath = firstCache.bindingPath;
+    let hasBinding = (firstCache.bindingPath != null);
+    let bindingSame = true;
+    let subBlock = firstLoader.subBlock;
+
+    for (let [key, loader] of it) {
+      let cache = loader.cache;
+      if (!cache) {
+        return notReadyState;
+      }
+      if (!Object.is(value, cache.value)) {
+        valueSame = false;
+      }
+      if (bindingPath !== cache.bindingPath) {
+        bindingSame = false;
+      }
+      if (cache.bindingPath != null) {
+        hasBinding = true;
+      }
+      if (!loader.subBlock) {
+        subBlock = false;
+      }
+    }
+    return {count, value, valueSame, bindingPath, hasBinding, bindingSame, subBlock};
+  }
+
+  getMenu = () => {
+    let {count, value, valueSame, bindingPath, hasBinding, bindingSame, subBlock} = this.getPropertyState();
+    return (
+      <Menu prefixCls="ant-dropdown-menu" selectable={false}>
+        <SubMenu title="Add Sub Block">
+          <Input size='small'/>
+        </SubMenu>
+        {hasBinding ?
+          <Menu.Item onClick={this.onUnbindClick}>
+            Unbind
+          </Menu.Item>
+          : null
+        }
+      </Menu>
+    );
+  };
+
   renderImpl() {
     let {conn, keys, funcDesc, propDesc, name} = this.props;
     let {unlocked, showSubBlock} = this.state;
 
     this.updateLoaders(keys, PropertyLoader);
 
-    ready: {
-      let it = this.loaders[Symbol.iterator]();
-      let [firstKey, firstLoader] = it.next().value;
-      let firstCache = firstLoader.cache;
-      if (!firstCache) {
-        break ready;
+    let onChange = propDesc.readonly ? null : this.onChange;
+
+    let {count, value, valueSame, bindingPath, hasBinding, bindingSame, subBlock} = this.getPropertyState();
+    if (count === 0) {
+      // not ready yet
+      return <div className='ticl-property'/>;
+    }
+    let inBoundClass;
+    if (subBlock) {
+      // inBoundClass = 'ticl-prop-inbound';
+    } else if (hasBinding) {
+      inBoundClass = 'ticl-prop-inbound';
+      if (!bindingSame) {
+        bindingPath = '???';
       }
+    }
 
-      let onChange = propDesc.readonly ? null : this.onChange;
-
-      let value = firstCache.value;
-      let valueSame = true;
-
-      let bindingPath = firstCache.bindingPath;
-      let hasBinding = (firstCache.bindingPath != null);
-      let bindingSame = true;
-      let subBlock = firstLoader.subBlock;
-
-      for (let [key, loader] of it) {
-        let cache = loader.cache;
-        if (!cache) {
-          break ready;
-        }
-        if (!Object.is(value, cache.value)) {
-          valueSame = false;
-        }
-        if (bindingPath !== cache.bindingPath) {
-          bindingSame = false;
-        }
-        if (cache.bindingPath != null) {
-          hasBinding = true;
-        }
-        if (!loader.subBlock) {
-          subBlock = false;
-        }
-      }
-
-      let inBoundClass;
-      if (subBlock) {
-        // inBoundClass = 'ticl-prop-inbound';
+    // lock icon
+    let locked = (hasBinding || !valueSame);
+    let renderLockIcon = locked && !propDesc.readonly;
+    let locktooltip: string;
+    if (renderLockIcon) {
+      if (unlocked) {
+        locktooltip = 'Unlocked for editing\nDouble click to lock';
       } else if (hasBinding) {
-        inBoundClass = 'ticl-prop-inbound';
-        if (!bindingSame) {
-          bindingPath = '???';
-        }
+        locktooltip = 'Editing blocked by binding\nDouble click to edit';
+      } else if (!valueSame) {
+        locktooltip = 'Inconsistent values\nDouble click to edit';
       }
+    }
 
-      // lock icon
-      let locked = (hasBinding || !valueSame);
-      let renderLockIcon = locked && !propDesc.readonly;
-      let locktooltip: string;
-      if (renderLockIcon) {
-        if (unlocked) {
-          locktooltip = 'Unlocked for editing\nDouble click to lock';
-        } else if (hasBinding) {
-          locktooltip = 'Editing blocked by binding\nDouble click to edit';
-        } else if (!valueSame) {
-          locktooltip = 'Inconsistent values\nDouble click to edit';
-        }
-      }
-
-      // expand icon
-      let renderSubBlock = subBlock && showSubBlock;
-      if (renderSubBlock && !this.subBlockKeys) {
-        this.buildSubBlockKeys(this.props);
-      }
+    // expand icon
+    let renderSubBlock = subBlock && showSubBlock;
+    if (renderSubBlock && !this.subBlockKeys) {
+      this.buildSubBlockKeys(this.props);
+    }
 
 
-      let editor: React.ReactNode;
-      let EditorClass = typeEditorMap[propDesc.type];
-      if (EditorClass) {
-        editor =
-          <EditorClass value={value} desc={propDesc} locked={locked && !unlocked} onChange={onChange}/>;
-      }
+    let editor: React.ReactNode;
+    let EditorClass = typeEditorMap[propDesc.type];
+    if (EditorClass) {
+      editor =
+        <EditorClass value={value} desc={propDesc} locked={locked && !unlocked} onChange={onChange}/>;
+    }
 
-      return (
-        <div className='ticl-property'>
-          {inBoundClass ? <div className={inBoundClass} title={bindingPath}/> : null}
+    return (
+      <div className='ticl-property'>
+        {inBoundClass ? <div className={inBoundClass} title={bindingPath}/> : null}
+        <Dropdown overlay={this.getMenu} trigger={['contextMenu']}>
           <div className={`ticl-property-name${propDesc.readonly ? ' ticl-property-readonly' : ''}`}
                draggable={true} onDragStart={this.onDragStart}
                onDragOver={this.onDragOver} onDrop={this.onDrop} onDragEnd={this.onDragEnd}>
             {translateProperty(funcDesc.name, name, funcDesc.ns)}
           </div>
-          {renderLockIcon ?
-            <Tooltip title={locktooltip} overlayClassName='ticl-tooltip'>
-              <Button shape='circle' tabIndex={-1} icon={unlocked ? 'edit' : 'lock'}
-                      onDoubleClick={this.unlock}> </Button>
-            </Tooltip>
-            : null}
-          {subBlock ?
-            <ExpandIcon opened={showSubBlock ? 'opened' : 'closed'} onClick={this.expandSubBlock}/>
-            : null
-          }
-          <div className='ticl-property-value'>
-            {editor}
-          </div>
-          {
-            renderSubBlock ?
-              <PropertyList conn={conn} keys={this.subBlockKeys}/>
-              : null
-          }
+        </Dropdown>
+        {renderLockIcon ?
+          <Tooltip title={locktooltip} overlayClassName='ticl-tooltip'>
+            <Button shape='circle' tabIndex={-1} icon={unlocked ? 'edit' : 'lock'}
+                    onDoubleClick={this.unlock}> </Button>
+          </Tooltip>
+          : null}
+        {subBlock ?
+          <ExpandIcon opened={showSubBlock ? 'opened' : 'closed'} onClick={this.expandSubBlock}/>
+          : null
+        }
+        <div className='ticl-property-value'>
+          {editor}
         </div>
-      );
-    }
-    // loaders are not ready
-    return <div className='ticl-property'/>;
+        {
+          renderSubBlock ?
+            <PropertyList conn={conn} keys={this.subBlockKeys}/>
+            : null
+        }
+      </div>
+    );
   }
 }
