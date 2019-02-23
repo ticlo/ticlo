5305880a17576dc1437b75cb7d3017d7b000cf14
Rick Zhou <rinick@gmail.com>
2019-02-22 23:44:05 -0800

    custom number editor

--- src/editor/property/value/NumberEditor.tsx
+++ src/editor/property/value/NumberEditor.tsx
@@ -1,33 +1,24 @@
 import * as React from "react";
-import {InputNumber} from "antd";
+import {Input, Button} from "antd";
 import {PropDesc} from "../../../common/block/Descriptor";
 import {ValueEditorProps} from "./ValueEditor";
-import {FocusEvent} from "react";
 
 export class NumberEditor extends React.PureComponent<ValueEditorProps, any> {
 
   // this is not a state bacause in commitChange() editorValue is changed but we don't want a re-render until prop change
-  _pendingValue: string | number = NaN;
+  _pendingValue: any = null;
 
-
-  commitChange(value: number) {
-    let {desc} = this.props;
-    if (desc.default === value) {
-      value = undefined;
-    }
-
-    this._pendingValue = NaN;
+  commitChange(value: string) {
+    this._pendingValue = null;
     this.props.onChange(value);
   }
 
-  onValueChange = (value: any) => {
+  onValueChange = (e: React.SyntheticEvent) => {
+    let value = (e.nativeEvent.target as HTMLInputElement).value;
     if (this._pendingTyping) {
-      if (value === value) {
-        // not allow NaN
-        if (value !== this.props.value || this._pendingValue === this._pendingValue) {
-          // when pending value already exists or server value is not the same
-          this._pendingValue = value;
-        }
+      if (value !== this.props.value || this._pendingValue != null) {
+        // when editorValue value already exists or server value is not the same
+        this._pendingValue = value;
       }
       this._pendingTyping = false;
       this.forceUpdate();
@@ -36,73 +27,50 @@ export class NumberEditor extends React.PureComponent<ValueEditorProps, any> {
     }
   };
 
-  checkAndCommit = (e?: FocusEvent) => {
-    let pendingValue = this._pendingValue;
-    if (pendingValue === pendingValue) {
-      if (typeof pendingValue === 'string') {
-        pendingValue = Number(pendingValue);
-      }
-      if (pendingValue === pendingValue) {
-        this.commitChange(pendingValue);
-      } else {
-        this._pendingValue = NaN;
-        this.forceUpdate();
-      }
-    } else if (!e) {
-      // when it's not blur event
-      this.commitChange(this.props.value);
+  onBlur = () => {
+    if (this._pendingValue != null) {
+      this.commitChange(this._pendingValue);
     }
   };
 
   _pendingTyping = false;
   onKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key.length === 1) { // regular typing
-      this._pendingTyping = true;
+    console.log(e.nativeEvent);
+    if (e.key === 'Escape') {
+      this._pendingTyping = false;
+      if (this._pendingValue != null) {
+        this._pendingValue = null;
+        this.forceUpdate();
+      }
+      return;
+    } else if (e.key === 'Enter') {
+      this._pendingTyping = false;
+      if (this._pendingValue != null) {
+        this.commitChange(this._pendingValue);
+      } else {
+        this.commitChange(this.props.value);
+      }
       return;
     }
-    switch (e.key) {
-      case 'Enter': {
-        this._pendingTyping = false;
-        this.checkAndCommit();
-        return;
-      }
-      case 'Escape': {
-        this._pendingTyping = false;
-        if (this._pendingValue) {
-          this._pendingValue = NaN;
-          this._pendingTyping = false;
-          this.forceUpdate();
-        }
-        return;
-      }
-      case 'ArrowUp':
-      case 'ArrowDown': {
-        this._pendingTyping = false;
-        return;
-      }
-      case 'Backspace':
-      case 'Delete': {
-        this._pendingTyping = true;
-        return;
-      }
-    }
+    this._pendingTyping = true;
+    e.stopPropagation();
   };
+
 
   render() {
     let {desc, value, locked, onChange} = this.props;
-    if (value === undefined && desc.default) {
-      value = desc.default;
-    }
-    if (this._pendingValue === this._pendingValue) { // not NaN
+    if (this._pendingValue != null) {
       value = this._pendingValue;
     } else if (locked) {
       onChange = null;
     }
+    let disabled = onChange == null;
     return (
-      <InputNumber size='small' placeholder={desc.placeholder} value={value}
-                   disabled={onChange == null}
-                   min={desc.min} max={desc.max} step={desc.step}
-                   onChange={this.onValueChange} onBlur={this.checkAndCommit} onKeyDown={this.onKeyDown}/>
+      <Input className='ticl-number-input' size='small' placeholder={desc.placeholder} value={value}
+             onChange={this.onValueChange} disabled={onChange == null}
+             addonBefore={<Button size='small' icon='minus' disabled={disabled}/>}
+             addonAfter={<Button size='small' icon='plus' disabled={disabled}/>}
+             onBlur={this.onBlur} onKeyDown={this.onKeyDown}/>
     );
   }
 }
