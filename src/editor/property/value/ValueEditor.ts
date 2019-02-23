f4b26a7ee291bbec189320831add7bff2877c1d9
Rick Zhou <rinick@gmail.com>
2019-02-22 23:43:29 -0800

    additional parameter in ValueEditor.onChange callback

--- src/editor/property/value/ValueEditor.ts
+++ src/editor/property/value/ValueEditor.ts
@@ -1,10 +1,11 @@
 import * as React from "react";
 import {PropDesc} from "../../../common/block/Descriptor";
 
+export type ChangeReason = 'minus' | 'plus' | 'enter' | 'blur';
 
 export interface ValueEditorProps {
   value: any;
   desc: PropDesc;
   locked?: boolean;
-  onChange?: (value: any) => void;
+  onChange?: (value: any, reason?: ChangeReason) => void;
 }
