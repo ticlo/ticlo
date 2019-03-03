87a1b665088f95b2b7db6d5a1dcced6357471f44
Rick Zhou <rinick@gmail.com>
2019-03-02 17:11:57 -0800

    add deepEqual function

--- src/common/util/Compare.ts
+++ src/common/util/Compare.ts
@@ -9,3 +9,44 @@ export function arrayEqual(a1: any[], a2: any[]) {
   }
   return true;
 }
+
+const isArray = Array.isArray;
+const keyList = Object.keys;
+
+export function deepEqual(a: any, b: any) {
+  if (Object.is(a, b)) {
+    return true;
+  }
+
+  if (a && b && typeof a === 'object' && typeof b === 'object') {
+    let arrA = isArray(a);
+    let arrB = isArray(b);
+
+    if (arrA && arrB) {
+      let length = a.length;
+      if (length !== b.length) return false;
+      for (let i = length; i-- !== 0;) {
+        if (!deepEqual(a[i], b[i])) return false;
+      }
+      return true;
+    }
+
+    if (arrA !== arrB) return false;
+
+    let keys = keyList(a);
+
+    if (keys.length !== keyList(b).length) {
+      return false;
+    }
+
+    for (let key of keys) {
+      if (!deepEqual(a[key], b[key])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  return false;
+}
+
--- src/common/util/spec/Compare.spec.ts
+++ src/common/util/spec/Compare.spec.ts
@@ -1,5 +1,5 @@
 import {assert} from "chai";
-import {arrayEqual} from '../Compare';
+import {arrayEqual, deepEqual} from '../Compare';
 
 describe("Compare", function () {
 
@@ -8,5 +8,23 @@ describe("Compare", function () {
     assert.isFalse(arrayEqual([1, 2], [2, 1]));
     assert.isFalse(arrayEqual([1, 2], [1, 2, 3]));
     assert.isFalse(arrayEqual([1, 2, 3], [1, 2]));
+  });
+
+  it('deepEqual', function () {
+    assert.isTrue(deepEqual(1, 1));
+    assert.isTrue(deepEqual(NaN, NaN));
+    assert.isTrue(deepEqual(undefined, undefined));
+    assert.isTrue(deepEqual('a', 'a'));
+    assert.isTrue(deepEqual(true, true));
+    assert.isTrue(deepEqual([1, 'a'], [1, 'a']));
+    assert.isTrue(deepEqual({a: 1}, {a: 1}));
+
+    assert.isFalse(deepEqual('a', 1));
+    assert.isFalse(deepEqual(null, undefined));
+    assert.isFalse(deepEqual([1], [1, 1]));
+    assert.isFalse(deepEqual([1], [2]));
+    assert.isFalse(deepEqual({a: 1}, {a: 2}));
+    assert.isFalse(deepEqual({a: 1, b: 1}, {a: 1}));
+    assert.isFalse(deepEqual({a: 1}, {a: 1, b: undefined}));
   });
 });
