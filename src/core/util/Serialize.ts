6a18f859b68a74354329e3fb5585e2cc6ee3d8a8
Rick Zhou <rinick@gmail.com>
2019-05-12 21:43:29 -0700

    add test for encode and decode

--- src/core/util/Serialize.ts
+++ src/core/util/Serialize.ts
@@ -4,8 +4,8 @@ import moment from 'moment';
 const startTime = moment();
 const Moment = startTime.constructor;
 
-function encodeMoment() {
-  return `\u001bTime:${(this as any).toISOString(true)}`;
+function encodeMoment(val: any): string {
+  return `\u001bTime:${val.toISOString(true)}`;
 }
 
 function decodeMoment(str: string): any {
@@ -13,14 +13,10 @@ function decodeMoment(str: string): any {
 }
 
 let encoder = new JsonEsc();
-encoder.registerRaw('Time', Moment, null, decodeMoment);
+encoder.registerRaw('Time', Moment, encodeMoment, decodeMoment);
 
 export function encode(value: any): string {
-  let keepToJSON = Moment.prototype.toJSON;
-  Moment.prototype.toJSON = encodeMoment;
-  let result = encoder.stringifySorted(value, 1);
-  Moment.prototype.toJSON = keepToJSON;
-  return result;
+  return encoder.stringifySorted(value, 1);
 }
 
 export function decode(str: string): any {
