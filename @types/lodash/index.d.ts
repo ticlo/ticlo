2e35be5dbb7b3561d0928b4f074c63a71cbefc7d
Rick Zhou <rinick@gmail.com>
2019-07-10 18:45:58 -0700

    remove @types/lodash dependency

---
+++ @types/lodash/index.d.ts
@@ -0,0 +1,13 @@
+declare module 'lodash/debounce' {
+  interface Debounce {
+    (): void;
+
+    cancel(): void;
+  }
+
+  export default function debounce(func: Function, wait: number, options?: any): Debounce;
+}
+
+declare module 'lodash/clamp' {
+  export default function clamp(n: number, low: number, high: number): number;
+}
