49589ebb30d577e0a38e996b172703f28d014a28
Rick Zhou <rinick@gmail.com>
2019-08-05 04:19:05 -0700

    test reconnect

---
+++ src/core/connect/spec/Reconnect.spec.ts
@@ -0,0 +1,73 @@
+import {assert} from "chai";
+import {Block, Root} from "../../block/Block";
+import {makeLocalConnection} from "../LocalConnection";
+import "../../functions/basic/Math";
+import {AsyncClientPromise} from "./AsyncClientPromise";
+import {shouldHappen, shouldReject} from "../../util/test-util";
+
+describe("Reconnect", function () {
+
+  it('reconnect', async function () {
+    let job = Root.instance.addJob('Reconnect1');
+    let [server, client] = makeLocalConnection(Root.instance, false);
+
+    job.setValue('o', 1);
+    job.setBinding('a', 'o');
+
+    let subcallbacks = new AsyncClientPromise();
+    client.subscribe('Reconnect1.a', subcallbacks);
+    let result = await subcallbacks.promise;
+    assert.equal(result.cache.value, 1);
+
+    let setcallbacks = new AsyncClientPromise();
+    client.setValue('Reconnect1.a', 3, setcallbacks);
+
+    let promiseReject = shouldReject(setcallbacks.promise);
+    client.onDisconnect();
+    await promiseReject; // setValue should receive error
+
+    job.setValue('a', 2);
+    result = await subcallbacks.promise;
+    assert.equal(result.cache.value, 2);
+    assert.isNull(result.cache.bindingPath);
+
+    // clean up
+    subcallbacks.cancel();
+    client.destroy();
+    Root.instance.deleteValue('Reconnect1');
+  });
+
+  it('watch object after reconnect ', async function () {
+    let job = Root.instance.addJob('Reconnect2');
+    let [server, client] = makeLocalConnection(Root.instance, false);
+
+    let child0 = job.createBlock('c0');
+
+    let callbacks1 = new AsyncClientPromise();
+    client.watch('Reconnect2', callbacks1);
+    let result1 = await callbacks1.promise;
+    assert.deepEqual(result1.cache, {'c0': child0._blockId});
+
+    client.onDisconnect();
+
+    let child1 = job.createBlock('c1');
+    job.deleteValue('c0');
+
+    let result2 = await callbacks1.promise;
+    assert.deepEqual(result2.cache, {'c1': child1._blockId});
+
+    // clean up
+    callbacks1.cancel();
+    client.destroy();
+    Root.instance.deleteValue('Reconnect2');
+  });
+
+  it('watch desc after reconnect ', async function () {
+    let job = Root.instance.addJob('Reconnect3');
+    let [server, client] = makeLocalConnection(Root.instance, true);
+
+
+    client.destroy();
+    Root.instance.deleteValue('Reconnect3');
+  });
+});
