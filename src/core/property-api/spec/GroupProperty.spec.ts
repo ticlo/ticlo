4ed5a09840a34d025465086c21290459b75a8c26
Rick Zhou <rinick@gmail.com>
2019-09-06 00:58:09 -0700

    test GroupProperty api

--- src/core/property-api/spec/GroupProperty.spec.ts
+++ src/core/property-api/spec/GroupProperty.spec.ts
@@ -2,11 +2,11 @@ import {assert} from "chai";
 
 import {Block, Job} from "../../block/Block";
 import "../../functions/basic/Math";
-import {setGroupLength} from "../GroupProperty";
+import {insertGroupProperty, moveGroupProperty, removeGroupProperty, setGroupLength} from "../GroupProperty";
 
-describe("PropertyUtil", function () {
+describe("GroupProperty", function () {
 
-  it('set group length', function () {
+  it('setGroupLength', function () {
     let job = new Job();
     job.load({
       '#is': 'add'
@@ -21,5 +21,50 @@ describe("PropertyUtil", function () {
 
     setGroupLength(job, '', 3);
     assert.deepEqual(job.getValue('@b-p'), ['0', '1', '2']);
+  });
+
+  it('insertGroupProperty', function () {
+    let job = new Job();
+    job.load({
+      '#is': 'add',
+      '0': 0,
+      '1': 1
+    });
+
+    insertGroupProperty(job, '', 0);
+    assert.equal(job.getValue('#len'), 3);
+    assert.equal(job.getValue('1'), 0);
+
+  });
+
+  it('removeGroupProperty', function () {
+    let job = new Job();
+    job.load({
+      '#is': 'add',
+      '0': 0,
+      '1': 1
+    });
+
+    removeGroupProperty(job, '', 0);
+    assert.equal(job.getValue('#len'), 1);
+    assert.equal(job.getValue('0'), 1);
+
+  });
+
+  it('moveGroupProperty', function () {
+    let job = new Job();
+    job.load({
+      '#is': 'add',
+      '0': 0,
+      '1': 1
+    });
+
+    moveGroupProperty(job, '', 0, 1);
+    assert.equal(job.getValue('0'), 1);
+    assert.equal(job.getValue('1'), 0);
+
+    moveGroupProperty(job, '', 1, 0);
+    assert.equal(job.getValue('0'), 0);
+    assert.equal(job.getValue('1'), 1);
   });
 });
