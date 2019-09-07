86c40ca4649b39f977028310227dded2937c4e64
Rick Zhou <rinick@gmail.com>
2019-09-07 00:54:16 -0700

    more group property api test

--- src/core/property-api/spec/GroupProperty.spec.ts
+++ src/core/property-api/spec/GroupProperty.spec.ts
@@ -21,6 +21,29 @@ describe("GroupProperty", function () {
 
     setGroupLength(job, '', 3);
     assert.deepEqual(job.getValue('@b-p'), ['0', '1', '2']);
+
+    // set -1 will use default length
+    setGroupLength(job, '', -1);
+    assert.deepEqual(job.getValue('@b-p'), ['0', '1']);
+
+    // invalid group, length should change but nothing else should happen
+    setGroupLength(job, 'aa', 3);
+    assert.deepEqual(job.getValue('aa#len'), 3);
+    assert.deepEqual(job.getValue('@b-p'), ['0', '1']);
+  });
+
+  it('setGroupLength on #more', function () {
+    let job = new Job();
+    job.load({
+      '#is': '',
+      '#more': [
+        {name: 'g', type: 'group', defaultLen: 2, properties: [{name: 'a', type: 'number'}]},
+      ]
+    });
+
+    setGroupLength(job, 'g', 3);
+    assert.deepEqual(job.getValue('g#len'), 3);
+    assert.deepEqual(job.getValue('@b-p'), ['a2']);
   });
 
   it('insertGroupProperty', function () {
@@ -35,6 +58,13 @@ describe("GroupProperty", function () {
     assert.equal(job.getValue('#len'), 3);
     assert.equal(job.getValue('1'), 0);
 
+    insertGroupProperty(job, '', 3);
+    assert.equal(job.getValue('#len'), 4);
+
+    // invalid index, no change
+    insertGroupProperty(job, '', -1);
+    insertGroupProperty(job, '', 100);
+    assert.equal(job.getValue('#len'), 4);
   });
 
   it('removeGroupProperty', function () {
@@ -49,6 +79,10 @@ describe("GroupProperty", function () {
     assert.equal(job.getValue('#len'), 1);
     assert.equal(job.getValue('0'), 1);
 
+    // invalid index, no change
+    removeGroupProperty(job, '', -1);
+    removeGroupProperty(job, '', 100);
+    assert.equal(job.getValue('#len'), 1);
   });
 
   it('moveGroupProperty', function () {
@@ -64,6 +98,12 @@ describe("GroupProperty", function () {
     assert.equal(job.getValue('1'), 0);
 
     moveGroupProperty(job, '', 1, 0);
+    assert.equal(job.getValue('0'), 0);
+    assert.equal(job.getValue('1'), 1);
+
+    // invalid index, no change
+    moveGroupProperty(job, '', 1, 100);
+    moveGroupProperty(job, '', 100, 0);
     assert.equal(job.getValue('0'), 0);
     assert.equal(job.getValue('1'), 1);
   });
