5a2f8a1faad8485e52fa041ae71373948c55aebd
Rick Zhou <rinick@gmail.com>
2019-09-27 01:21:48 -0700

    use bigint for the uid

--- src/core/util/Uid.ts
+++ src/core/util/Uid.ts
@@ -3,8 +3,7 @@
 // (Number.MAX_SAFE_INTEGER-1).toString(36) == 2gosa7pa2gu
 
 export class Uid {
-  _count = 0;
-  _prefix?: Uid;
+  _count = 0n;
 
   _current: string = '0';
 
@@ -13,19 +12,8 @@ export class Uid {
   }
 
   next(): string {
-    if (++this._count === Number.MAX_SAFE_INTEGER) {
-      this._count = 0;
-      if (!this._prefix) {
-        this._prefix = new Uid();
-      } else {
-        this._prefix.next();
-      }
-    }
-    if (this._prefix) {
-      this._current = `${this._prefix.current}-${this._count.toString(36)}`;
-    } else {
-      this._current = this._count.toString(36);
-    }
+    ++this._count;
+    this._current = this._count.toString(36);
     return this._current;
   }
 }
--- src/core/util/spec/Uid.spec.ts
+++ src/core/util/spec/Uid.spec.ts
@@ -8,16 +8,6 @@ describe("Uid", function () {
 
     assert.equal(uid.current, '0', 'initial value');
     assert.equal(uid.next(), '1', 'next value');
-
-    uid._count = Number.MAX_SAFE_INTEGER - 1;
-    assert.equal(uid.next(), '0-0', 'create prefix when max safe int reached');
-
-    uid._count = Number.MAX_SAFE_INTEGER - 1;
-    assert.equal(uid.next(), '1-0', 'add prefix when max safe int reached');
-
-    uid._count = Number.MAX_SAFE_INTEGER - 1;
-    uid._prefix._count = Number.MAX_SAFE_INTEGER - 1;
-    assert.equal(uid.next(), '0-0-0', 'multi-prefix when max safe int reached');
   });
 
 });
