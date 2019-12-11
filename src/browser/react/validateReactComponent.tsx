a62bca0d516a3ec5f83dfb77d3e18605691723c8
Rick Zhou <rinick@gmail.com>
2019-12-11 00:13:32 -0800

    validateReactComponent should allow Array input

--- src/browser/react/validateReactComponent.tsx
+++ src/browser/react/validateReactComponent.tsx
@@ -2,12 +2,31 @@ import React from 'react';
 
 const reactTypeof = ((<div />) as any).$$typeof;
 
-export function validateReactComponent(component: any) {
+function validateSingleComponent(component: any) {
   if (component && typeof component === 'object' && component.$$typeof !== reactTypeof) {
-    component = null;
+    return null;
   }
+  return component;
+}
+
+export function validateReactComponent(component: any) {
   if (component == null) {
-    return "";
+    return '';
+  } else if (component && typeof component === 'object') {
+    if (Array.isArray(component)) {
+      let result: any[] = [];
+      for (let child of component) {
+        let validatedChild = validateSingleComponent(child);
+        if (validatedChild != null) {
+          result.push(validatedChild);
+        }
+      }
+      return result;
+    }
+    if (component.$$typeof !== reactTypeof) {
+      return '';
+    }
   }
+
   return component;
 }
