4b4529ed7dbf8f4e7918ef6ad05ca7bbedbb5bb7
Rick Zhou <rinick@gmail.com>
2020-01-12 11:11:31 -0800

    more FileJobLoader tests

--- src/node/loader/spec/FileJobLoader.spec.ts
+++ src/node/loader/spec/FileJobLoader.spec.ts
@@ -12,6 +12,7 @@ import {FunctionDesc} from '../../../core/block/Descriptor';
 import {Types} from '../../../core/block/Type';
 import {FileJobLoader} from '../FileJobLoader';
 import {MockWsServer} from '../../connect/spec/MockWsServer';
+import {decode} from '../../../core';
 
 describe('FileJobLoader', function() {
   before(function() {
@@ -32,16 +33,44 @@ describe('FileJobLoader', function() {
     await waitTick(10);
     assert.isFalse(Fs.existsSync(path));
 
+    // overwrite multiple times
+    job = root.addJob('job2');
+    job.applyChange();
+    job.setValue('value', 123);
+    job.applyChange();
+    job.setValue('value', 456);
+    job.applyChange();
+    await waitTick(15);
+    savedData = Fs.readFileSync('./temp/jobLoaderTest/job2.ticlo', 'utf8');
+    assert.deepEqual(decode(savedData), {'#is': '', 'value': 456});
+
+    // overwrite delete after write
+    job = root.addJob('job3');
+    job.applyChange();
+    root.deleteJob('job3');
+    job = root.addJob('job3');
+    root.deleteJob('job3');
+    await waitTick(15);
+    assert.isFalse(Fs.existsSync('./temp/jobLoaderTest/job3.ticlo'));
+
+    // overwirte delete after delete
+    job = root.addJob('job4');
+    root.deleteJob('job4');
+    job = root.addJob('job4');
+    root.deleteJob('job4');
+    await waitTick(15);
+    assert.isFalse(Fs.existsSync('./temp/jobLoaderTest/job4.ticlo'));
+
     root.destroy();
   });
   it('init loader', async function() {
-    const path = './temp/jobLoaderTest/job2.ticlo';
+    const path = './temp/jobLoaderTest/job5.ticlo';
     Fs.writeFileSync(path, '{"#is":"", "value":321}');
 
     let root = new Root();
     root.setLoader(new FileJobLoader('./temp/jobLoaderTest'));
 
-    assert.equal(root.queryValue('job2.value'), 321);
+    assert.equal(root.queryValue('job5.value'), 321);
 
     Fs.unlinkSync(path);
     root.destroy();
