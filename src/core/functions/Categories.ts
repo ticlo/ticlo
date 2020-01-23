1521f898525eb1c91ac2d0ff4cf1cb6542bf07e3
Rick Zhou <rinick@gmail.com>
2020-01-22 22:29:44 -0800

    add icons to categories

--- src/core/functions/Categories.ts
+++ src/core/functions/Categories.ts
@@ -5,3 +5,33 @@ Functions.addCategory({
   name: 'math',
   icon: 'fas:plus'
 });
+
+Functions.addCategory({
+  id: 'string',
+  name: 'string',
+  icon: 'txt:str'
+});
+
+Functions.addCategory({
+  id: 'string',
+  name: 'string',
+  icon: 'txt:str'
+});
+
+Functions.addCategory({
+  id: 'compare',
+  name: 'compare',
+  icon: 'fas:equals'
+});
+
+Functions.addCategory({
+  id: 'http',
+  name: 'http',
+  icon: 'fas:network-wired'
+});
+
+Functions.addCategory({
+  id: 'other',
+  name: 'other',
+  icon: 'fas:question'
+});
