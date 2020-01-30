77ef38c2c3772025de548bbabb351b153edf300f
Rick Zhou <rinick@gmail.com>
2020-01-30 01:13:51 -0800

    implement CreateStyle function

--- src/html/functions/CreateStyle.ts
+++ src/html/functions/CreateStyle.ts
@@ -5,28 +5,12 @@ export class CreateStyleFunction extends BaseFunction {
   run() {
     let spread = this._data.getValue('#spread');
     let output = spread ? {...spread} : {};
-    let custom: PropDesc | PropGroupDesc[] = this._data.getValue('#custom');
-    if (Array.isArray(custom)) {
-      for (let prop of custom) {
-        if (prop) {
-          if (prop.type === 'group') {
-            let length = this._data.getValue(`${prop.name}#len`);
-            if (!(length >= 0)) {
-              length = prop.defaultLen;
-            }
-            for (let groupProp of prop.properties) {
-              let arr: any[] = [];
-              for (let i = 0; i < length; ++i) {
-                arr.push(this._data.getValue(`${groupProp.name}${i}`));
-              }
-              output[groupProp.name] = arr;
-            }
-          } else {
-            let val = this._data.getValue(prop.name);
-            if (val !== undefined) {
-              output[prop.name] = this._data.getValue(prop.name);
-            }
-          }
+    let optional: string[] = this._data.getValue('#optional');
+    if (Array.isArray(optional)) {
+      for (let field of optional) {
+        let value = this._data.getValue(field);
+        if (value !== undefined) {
+          output[field] = value;
         }
       }
     }
