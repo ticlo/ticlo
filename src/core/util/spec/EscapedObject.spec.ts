c31ddc9e60b26947e8e859f5979e93abb406358f
Rick Zhou <rinick@gmail.com>
2020-03-02 00:39:11 -0800

    test escaped object

---
+++ src/core/util/spec/EscapedObject.spec.ts
@@ -0,0 +1,12 @@
+import {assert} from 'chai';
+import {decodeUnknown, EscapedObject} from '../EscapedObject';
+
+describe('EscapedObject', function() {
+  const encodedString = '"\\u001b:title"';
+  it('basic', function () {
+    let esc = new EscapedObject('title');
+    assert.equal(JSON.stringify(esc), encodedString);
+
+    assert.equal(decodeUnknown(JSON.parse(encodedString)).title , 'title');
+  });
+});
