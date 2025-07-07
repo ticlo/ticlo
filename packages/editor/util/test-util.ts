cfd6516cb8732ae586f33d42b8fad0960bad1bb1
Rick Zhou <rinick@gmail.com>
2025-07-07 01:58:15 -0700

    fix css file paths

--- packages/editor/util/test-util.ts
+++ packages/editor/util/test-util.ts
@@ -17,9 +17,9 @@ function loadCssInHeader() {
   if (!(window as any).ticloCssLoaded) {
     loadOneCss('https://fonts.googleapis.com/css?family=Fredoka+One');
     // For Vite, we need to use the correct paths
-    loadOneCss('/app/antd.css');
-    loadOneCss('/app/editor.css');
-    loadOneCss('/app/icons.css');
+    loadOneCss('/app/css/antd.css');
+    loadOneCss('/app/css/editor.css');
+    loadOneCss('/app/css/icons.css');
     (window as any).ticloCssLoaded = true;
   }
 }
