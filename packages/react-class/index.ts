45ba43422804013cd360018e9312b406a77097ca
rinick <rinick@gmail.com>
2025-12-25 19:04:07 -0800

    rename react-hook to react

---
+++ packages/react-class/index.ts
@@ -0,0 +1,15 @@
+import {Functions} from '@ticlo/core';
+import './HtmlAttributes.js';
+import './Jsx.js';
+import './RenderDom.js';
+import './elements/CommonElements.js';
+import './elements/ImgElement.js';
+import './elements/InputElement.js';
+
+Functions.addCategory({
+  id: 'react:',
+  name: 'react',
+  icon: 'fab:react',
+  color: '5ce',
+  ns: 'react',
+});
--- packages/react/index.ts
+++ packages/react/index.ts
@@ -1,15 +1,12 @@
-import {Functions} from '@ticlo/core';
-import './HtmlAttributes.js';
-import './Jsx.js';
-import './RenderDom.js';
-import './elements/CommonElements.js';
-import './elements/ImgElement.js';
-import './elements/InputElement.js';
-
-Functions.addCategory({
-  id: 'react:',
-  name: 'react',
-  icon: 'fab:react',
-  color: '5ce',
-  ns: 'react',
-});
+export {useBlockProps} from './hooks/useBlockProps.js';
+export {useChildren} from './hooks/useChildren.js';
+export {useFilteredBlocks} from './hooks/useFilteredBlocks.js';
+export {useWatchBlock} from './hooks/useWatchBlock.js';
+export {Values} from './types/Values.js';
+export {
+  registerComponent,
+  findComponent,
+  renderChildren,
+  TicloFuncComp,
+  isContainerFunction,
+} from './types/Component.js';
